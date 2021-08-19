a6bff0c9d3163282691b6674006e18a05d69c1a8
aumugisha <arthur.umugisha@gmail.com>
2021-08-19 22:12:35 +0200

    courses actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:show, :edit, :update] 
+
+  resources :subscriptions, except: [:edit]  
+  
+  resources :courses, only: [:index, :show] do
+    resources :chapters, only: [:index, :show]
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
