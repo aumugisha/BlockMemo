48c605bc21568f4374249491ea671728252f7683
SinouhÃe Ferreira Monteiro Nunes <sinouhe.monteironunes@gmail.com>
2021-08-21 12:59:59 +0200

    Seed file: Chapter,Course and User

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'sinouhe.monteironunes@gmail.com', password: '123456', password_confirmation: '123456')
+Course.create(name: 'Blockchain fundamentals')
+Chapter.create(name: 'Why Blockchain?', level:1, content: '',
+summary: 'In 2010, a programmer paid 
+10,000 Bitcoins for 2 pizzas, roughly worth $30', order: 1, course_id: 1)
+
+
