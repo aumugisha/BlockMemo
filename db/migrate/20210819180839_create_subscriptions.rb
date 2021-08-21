61bd9beedb63e6bb843f8dd1f7c77c7c13d7f13a
aumugisha <arthur.umugisha@gmail.com>
2021-08-21 16:41:36 +0200

    add default value du subscription table

--- db/migrate/20210819180839_create_subscriptions.rb
+++ db/migrate/20210819180839_create_subscriptions.rb
@@ -1,8 +1,8 @@
 class CreateSubscriptions < ActiveRecord::Migration[6.0]
   def change
     create_table :subscriptions do |t|
-      t.integer :chapter_completed
-      t.boolean :completed
+      t.integer :chapter_completed, default: 0
+      t.boolean :completed, default: false
       t.references :course, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
