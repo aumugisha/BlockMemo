5edcf507669a9107365608c029126b51db2644d2
SinouhÃe Ferreira Monteiro Nunes <sinouhe.monteironunes@gmail.com>
2021-09-04 17:08:49 +0200

    Submit reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,31 @@
 class ReviewsController < ApplicationController
+  before_action :set_course, only: [:new, :create]
+
   def new
     # we need @course in our `simple_form_for`
     @course = Course.find(params[:course_id])
     authorize @course, :show?
     @review = @course.reviews.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    # we need `course_id` to associate review with corresponding restaurant
+    @course = Course.find(params[:course_id])
+    @review.course = @course
+    authorize @course, :show?
+    @review.save
+    redirect_to course_path(@course)
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:course_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:comment)
   end
 end
 
---
+++ app/policies/review_policy.rb
@@ -0,0 +1,5 @@
+class CoursePolicy < ApplicationPolicy
+  def create?
+    true
+  end
+end
