5fa5268989f77724edcf3ab266cc255223c4e975
aumugisha <arthur.umugisha@gmail.com>
2021-09-09 22:06:31 +0200

    improved backend

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: [ :index,  ]
+  
   def index
     if params[:query].present?
       @courses = Course.where("name ILIKE ?", "%#{params[:query]}%")
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -7,25 +7,26 @@ class TestsController < ApplicationController
     end
 
     def update
-
         @test = Test.find(params[:id])
         @test.choice = params[:test][:choice]
+        @subs = Subscription.where("user_id = ? AND course_id = ?", current_user.id, @test.chapter.course.id).first
         if @test.choice == @test.correct_answer
             @test.passed = true
-            @subs = Subscription.where("user_id = ? AND course_id = ?", current_user.id, @test.chapter.course.id).first
             @subs.chapter_completed += 1
-            @subs.save
-            if @subs.chapter_completed < @subs.course.chapters.last.order
-                redirect_to course_chapter_path(@test.chapter.course, Chapter.find_by(order: @subs.chapter_completed + 1))
-              else
+            if @subs.chapter_completed <= @subs.course.chapters.last.order
+                redirect_to course_chapter_path(@test.chapter.course, Chapter.find_by(order: @subs.chapter_completed))
+            else
+                @subs.chapter_completed += 1
+                @subs.completed = true
                 redirect_to dashboard_path
-              end
+            end
         else
             redirect_to course_chapter_path(@test.chapter.course,@test.chapter)
             
         end
+        @subs.save
         authorize @test
     end
-
+    
 end
 
